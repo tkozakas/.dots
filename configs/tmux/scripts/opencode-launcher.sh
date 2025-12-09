eb356e33e73a08e9cfa56321c712585abb2e0624
tkozakas <tomas.kozakas@gmail.com>
2025-12-09 21:56:49 +0200

    Update

--- configs/tmux/scripts/opencode-launcher.sh
+++ configs/tmux/scripts/opencode-launcher.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 PANE_WIDTH=65
-PANE_TITLE="opencode"
 
 toggle_opencode() {
-    opencode_pane=$(tmux list-panes -F '#{pane_id} #{pane_title}' | grep "$PANE_TITLE" | awk '{print $1}')
+    opencode_pane=$(tmux list-panes -F '#{pane_id} #{pane_current_command}' | grep "opencode" | awk '{print $1}')
     if [[ -n "$opencode_pane" ]]; then
         tmux kill-pane -t "$opencode_pane"
     else
         tmux split-window -h -l "$PANE_WIDTH" -c "#{pane_current_path}" "opencode"
-        tmux select-pane -T "$PANE_TITLE"
     fi
 }
 
