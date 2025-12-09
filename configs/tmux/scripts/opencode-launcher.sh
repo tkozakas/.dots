f450ddee71b4486a2126236c711f6494a2196fb6
Tomas Kozakas <tomas.kozakas@vinted.com>
2025-12-09 14:27:27 +0200

    Fix

--- configs/tmux/scripts/opencode-launcher.sh
+++ configs/tmux/scripts/opencode-launcher.sh
@@ -5,7 +5,6 @@ PANE_TITLE="opencode"
 
 toggle_opencode() {
     opencode_pane=$(tmux list-panes -F '#{pane_id} #{pane_title}' | grep "$PANE_TITLE" | awk '{print $1}')
-    
     if [[ -n "$opencode_pane" ]]; then
         tmux kill-pane -t "$opencode_pane"
     else
