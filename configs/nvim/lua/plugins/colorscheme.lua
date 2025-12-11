10f5cdf7dd7d7a80db7b4149918abb0f174ad917
Tomas Kozakas <tomas.kozakas@vinted.com>
2025-12-11 10:09:07 +0200

    add theme

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
-  config = function()
-    require('rose-pine').setup({
-      variant = 'main',
-      disable_background = true,
-      styles = {
-        italic = false,
-      },
-    })
-    vim.cmd('colorscheme rose-pine')
-  end,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("rose-pine").setup({
+            disable_background = true,
+        })
+        vim.cmd("colorscheme rose-pine")
+    end,
 }
