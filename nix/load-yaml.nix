a45fd426190a10a6120b14f5e0bc04fc5fb82b40
tkozakas <tomas.kozakas@gmail.com>
2026-04-19 21:21:06 +0300

    switch config from yaml to json

--- nix/load-yaml.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, lib }:
-yamlPath:
-let
-  json = pkgs.runCommand "config.json" { } ''
-    ${pkgs.yq-go}/bin/yq -o=json '.' ${yamlPath} > $out
-  '';
-in
-builtins.fromJSON (builtins.readFile json)
