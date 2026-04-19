baadb3923e666e35ec46f89a8f96cfb40a2bbd10
tkozakas <tomas.kozakas@gmail.com>
2026-04-19 20:44:30 +0300

    pin determinate nix installer version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+NIX_INSTALLER_VERSION=v3.17.3
 NIX_DAEMON_SH=/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 NIX_USER_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 if ! command -v nix >/dev/null 2>&1; then
-  curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  curl -fsSL "https://install.determinate.systems/nix/tag/${NIX_INSTALLER_VERSION}" | sh -s -- install --no-confirm
   if   [ -e "$NIX_DAEMON_SH" ]; then . "$NIX_DAEMON_SH"
   elif [ -e "$NIX_USER_SH"   ]; then . "$NIX_USER_SH"
   fi
