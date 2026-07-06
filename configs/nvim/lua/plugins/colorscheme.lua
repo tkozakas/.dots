aff89db89704d52e391f91629aa849f534a69803
Tomas Kozakas <tomas.kozakas@vinted.com>
2026-07-06 18:38:21 +0300

    nvim: restore colorcolumn guide to grey

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -27,8 +27,8 @@ local function minimal_chrome()
   -- Fully mute any residual EndOfBuffer glyph
   set(0, 'EndOfBuffer',  { ctermfg = 0, ctermbg = 'NONE', cterm = {} })
   -- Kill the reversed (solid white) ColorColumn block at column 80; render it
-  -- as a very subtle dark cell so it barely reads past the line end.
-  set(0, 'ColorColumn',  { ctermfg = 'NONE', ctermbg = 0, cterm = {} })
+  -- as a subtle gray cell instead of an inverted bar past the line end.
+  set(0, 'ColorColumn',  { ctermfg = 'NONE', ctermbg = 8, cterm = {} })
 end
 
 vim.api.nvim_create_autocmd('ColorScheme', {
